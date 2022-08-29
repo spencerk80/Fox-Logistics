6237e179ec6b4c1930343b0012788c0bc699656d
Kristoffer Spencer <whobanil80@linuxmail.org>
2022-08-29 09:41:05 -0700

    Forgot repo annotations...

--- ReimbursementAPI/src/main/java/com/github/spencerk/ReimbursementAPI/repository/EmployeeRepository.java
+++ ReimbursementAPI/src/main/java/com/github/spencerk/ReimbursementAPI/repository/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.github.spencerk.ReimbursementAPI.repository;
 
 import com.github.spencerk.ReimbursementAPI.Entity.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EmployeeRepository extends MongoRepository<Employee, UUID> {
 }
--- ReimbursementAPI/src/main/java/com/github/spencerk/ReimbursementAPI/repository/ReimbursementTicketRepository.java
+++ ReimbursementAPI/src/main/java/com/github/spencerk/ReimbursementAPI/repository/ReimbursementTicketRepository.java
@@ -2,8 +2,10 @@ package com.github.spencerk.ReimbursementAPI.repository;
 
 import com.github.spencerk.ReimbursementAPI.Entity.ReimbursementTicket;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ReimbursementTicketRepository extends MongoRepository<ReimbursementTicket, UUID> {
 }
